023e578a2f9ca3a06c7679bfc9d54c7c59c3eb50
patrick <patrick.schliesser@vitra.com>
2024-08-05 15:55:47 +0200

    Refactoring

--- app.py
+++ app.py
@@ -8,13 +8,8 @@ app = Flask(__name__)
 def home():
     if request.method == "POST":
         link_recieved = request.form["input_link"]
-        print(link_recieved)
-       
         link_short = pyshorteners.Shortener().tinyurl.short(link_recieved)
         return render_template("index.html", link_out=link_short, link_in=link_recieved)
-
-
-        
     else:
         return render_template("index.html")
 
